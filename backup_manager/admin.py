d58ec917c9e60d062d015f881ce272baf1b37d3a
Pedro Henrique Gonçalves Pires <pedro.pires@defensoria.mg.def.br>
2023-09-12 15:29:30 -0300

    Add admin models view

--- backup_manager/admin.py
+++ backup_manager/admin.py
@@ -3,7 +3,35 @@ from django.contrib import admin
 from backup_manager.models import Environment, Project, Backup, Restore
 
 # Register your models here.
-admin.site.register(Project)
-admin.site.register(Environment)
-admin.site.register(Backup)
-admin.site.register(Restore)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'backup_path')
+    search_fields = ('name',)
+
+
+admin.site.register(Project, ProjectAdmin)
+
+
+class EnvironmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'host_db', 'port_db', 'backup_path')
+    search_fields = ('name', 'host_db')
+
+
+admin.site.register(Environment, EnvironmentAdmin)
+
+
+class BackupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'path', 'project', 'environment', 'dt_create')
+    search_fields = ('name', 'path', 'project__name', 'environment__name')
+
+
+admin.site.register(Backup, BackupAdmin)
+
+
+class RestoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'origin_backup', 'destination_environment', 'dt_restore')
+    search_fields = ('name', 'origin_backup__name', 'destination_environment__name')
+
+
+admin.site.register(Restore, RestoreAdmin)
