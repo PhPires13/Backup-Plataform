18bf5e8abc2dfbc15bb8db580be70374b89b4814
Pedro Henrique Gonçalves Pires <pedro.pires@defensoria.mg.def.br>
2023-09-12 15:07:17 -0300

    Define db_table for models

--- backup_manager/models.py
+++ backup_manager/models.py
@@ -7,12 +7,18 @@ class Project(models.Model):
     name = models.CharField(max_length=255)
     backup_path = models.CharField(max_length=255)
 
+    class Meta:
+        db_table = 'tb_project'
+
 
 class Environment(models.Model):
     name = models.CharField(max_length=255)
     host_db = models.CharField(max_length=255)
     port_db = models.IntegerField()
     backup_path = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'tb_environment'
 
 
 class Backup(models.Model):
@@ -22,9 +28,15 @@ class Backup(models.Model):
     environment = models.ForeignKey(Environment, on_delete=models.CASCADE)
     dt_create = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        db_table = 'tb_backup'
+
 
 class Restore(models.Model):
     name = models.CharField(max_length=255)
     origin_backup = models.ForeignKey(Backup, on_delete=models.CASCADE)
     destination_environment = models.ForeignKey(Environment, on_delete=models.CASCADE)
     dt_restore = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'tb_restore'
