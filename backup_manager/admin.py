00fcb4040e7b9a5e0461498d4f433af0d42a63fb
Pedro Henrique Gonçalves Pires <pedro.pires@defensoria.mg.def.br>
2023-09-12 15:16:33 -0300

    Registra os models no admin

--- backup_manager/admin.py
+++ backup_manager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from backup_manager.models import Environment, Project, Backup, Restore
+
 # Register your models here.
+admin.site.register(Project)
+admin.site.register(Environment)
+admin.site.register(Backup)
+admin.site.register(Restore)
