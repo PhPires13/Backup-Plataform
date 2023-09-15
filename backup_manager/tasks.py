c7174584fd7cd803d2c1280f666d19a5e1a0a942
Pedro Henrique Gonçalves Pires <pedro.pires@defensoria.mg.def.br>
2023-09-15 15:54:04 -0300

    Cria file para tasks

---
+++ backup_manager/tasks.py
@@ -0,0 +1,11 @@
+import os
+
+from backup_manager.models import Database, Backup
+
+
+def perform_backup(database: Database):
+    pass
+
+
+def perform_restore(origin_backup: Backup, setination_database: Database):
+    pass
