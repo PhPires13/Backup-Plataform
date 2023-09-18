0c3062347e457c55353b631d04f7c66db7330531
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2023-09-18 12:21:08 -0300

    Cria modularização de run command

--- backup_manager/tasks.py
+++ backup_manager/tasks.py
@@ -7,6 +7,27 @@ from celery import shared_task
 from backup_manager.models import Database, Backup, Restore
 
 
+def run_command(obj, command: list, password: str):
+    # Set the status and description before running the command
+    obj.set_status('ST')
+    obj.dt_start = datetime.now()
+    obj.save()
+
+    # Run the command
+    try:
+        result = subprocess.run(command, input=password.encode(), check=True, text=True)
+        # Set the status and description after a success
+        obj.set_status('SC')
+        obj.description = result.stdout
+    except subprocess.CalledProcessError as e:
+        # Set the status and description after a fail
+        obj.set_status('FL')
+        obj.description = e.stderr
+
+    obj.dt_end = datetime.now()  # Set the end date
+    obj.save()
+
+
 @shared_task
 def perform_backup(backup: Backup, user: str, password: str):
     host = backup.database.host
