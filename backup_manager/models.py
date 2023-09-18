af31a70e3580d15d1f8d8e8f70d2821385747741
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2023-09-18 11:32:54 -0300

    Rename backup_path to complete_path

--- backup_manager/models.py
+++ backup_manager/models.py
@@ -64,7 +64,7 @@ class Backup(models.Model):
     def __str__(self):
         return f'{self.name} ({self.database}) [{self.dt_create}]'
 
-    def backup_path(self) -> str:
+    def complete_path(self) -> str:
         default = os.path.join('/', 'mnt', 'netapp01', 'postgres')
         month_year = self.dt_create.strftime('%m-%Y')
         path = os.path.join(default, self.database.environment.name, month_year, self.database.project.name, self.path)
