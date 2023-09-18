09a16d219f4c914c41e44691c86b2e96361fccc7
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2023-09-18 12:43:23 -0300

    Add celery to settings.py

--- plataforma_backup/settings.py
+++ plataforma_backup/settings.py
@@ -134,3 +134,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Celery configuration
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+# Optional: Configure concurrency (number of workers and concurrency level)
+# CELERY_WORKER_CONCURRENCY = 4  # Number of worker processes
