b25b79062184b186ca34bf74c4d5043436384a1d
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2023-09-18 12:22:14 -0300

    Aplica modularização de run command

--- backup_manager/tasks.py
+++ backup_manager/tasks.py
@@ -44,23 +44,7 @@ def perform_backup(backup: Backup, user: str, password: str):
         database.name
     ]
 
-    backup.set_status('ST')
-    backup.dt_start = datetime.now()
-    backup.save()
-
-    # Run the pg_dump command
-    try:
-        result = subprocess.run(command, input=password.encode(), check=True, text=True)
-        # Set the status and description after a successful backup
-        backup.set_status('SC')
-        backup.description = result.stdout
-    except subprocess.CalledProcessError as e:
-        # Set the status and description after a failed backup
-        backup.set_status('FL')
-        backup.description = e.stderr
-
-    backup.dt_end = datetime.now()
-    backup.save()
+    run_command(backup, command, password)
 
 
 @shared_task
@@ -81,20 +65,4 @@ def perform_restore(restore: Restore, user: str, password: str):
         origin_backup.complete_path()
     ]
 
-    restore.set_status('ST')
-    restore.dt_start = datetime.now()
-    restore.save()
-
-    # Run the pg_restore command
-    try:
-        result = subprocess.run(command, input=password.encode(), check=True, text=True)
-        # Set the status and description after a successful restore
-        restore.set_status('SC')
-        restore.description = result.stdout
-    except subprocess.CalledProcessError as e:
-        # Set the status and description after a failed restore
-        restore.set_status('FL')
-        restore.description = e.stderr
-
-    restore.dt_end = datetime.now()
-    restore.save()
+    run_command(restore, command, password)
