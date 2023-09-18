356bfe6da1336a682389951f2dbae126e8abd430
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2023-09-18 12:16:51 -0300

    Create task to perform restore

--- backup_manager/tasks.py
+++ backup_manager/tasks.py
@@ -44,4 +44,36 @@ def perform_backup(backup: Backup, user: str, password: str):
 
 @shared_task
 def perform_restore(restore: Restore, user: str, password: str):
-    pass
+    origin_backup = restore.origin_backup
+    destination_database = restore.destination_database
+
+    host = destination_database.host
+
+    # Construct the pg_restore command
+    command = [
+        'pg_restore',
+        '-h', host.ip,
+        '-p', str(host.port),
+        '-U', user,
+        '--no-password',  # Use this flag to prompt for a password
+        '--dbname', destination_database.name,
+        origin_backup.complete_path()
+    ]
+
+    restore.set_status('ST')
+    restore.dt_start = datetime.now()
+    restore.save()
+
+    # Run the pg_restore command
+    try:
+        result = subprocess.run(command, input=password.encode(), check=True, text=True)
+        # Set the status and description after a successful restore
+        restore.set_status('SC')
+        restore.description = result.stdout
+    except subprocess.CalledProcessError as e:
+        # Set the status and description after a failed restore
+        restore.set_status('FL')
+        restore.description = e.stderr
+
+    restore.dt_end = datetime.now()
+    restore.save()
