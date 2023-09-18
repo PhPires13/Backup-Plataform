344effe0d3cf6b333ce274d0619f0fcbc58cb2c2
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2023-09-18 11:42:03 -0300

    Cria task perform backup

--- backup_manager/tasks.py
+++ backup_manager/tasks.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+from datetime import datetime
 
 from celery import shared_task
 
@@ -7,7 +9,35 @@ from backup_manager.models import Database, Backup, Restore
 
 @shared_task
 def perform_backup(backup: Backup, user: str, password: str):
-    pass
+    host = backup.database.host
+    database = backup.database
+
+    # Construct the pg_dump command
+    command = [
+        'pg_dump',
+        '-h', host.ip,
+        '-p', str(host.port),
+        '-U', user,
+        '--no-password',  # Use this flag to prompt for a password
+        '--file', backup.complete_path(),
+        database.name
+    ]
+
+    # Run the pg_dump command
+    try:
+        backup.dt_start = datetime.now()
+        backup.save()
+        result = subprocess.run(command, input=password.encode(), check=True, text=True)
+        # Set the status and end time after a successful backup
+        backup.status = True
+        backup.description = result.stdout
+    except subprocess.CalledProcessError as e:
+        # Set the status and end time after a failed backup
+        backup.status = False
+        backup.description = e.stderr
+
+    backup.dt_end = datetime.now()
+    backup.save()
 
 
 @shared_task
