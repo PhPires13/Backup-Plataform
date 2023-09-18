21752063599179dd6825c4b2c381dde1ad46f1db
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2023-09-18 11:27:46 -0300

    Marca as tasks como @shared_tasks

--- backup_manager/tasks.py
+++ backup_manager/tasks.py
@@ -1,11 +1,15 @@
 import os
 
+from celery import shared_task
+
 from backup_manager.models import Database, Backup, Restore
 
 
+@shared_task
 def perform_backup(backup: Backup, user: str, password: str):
     pass
 
 
+@shared_task
 def perform_restore(restore: Restore, user: str, password: str):
     pass
