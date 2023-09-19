da719608818a4d0c4ed73acc766f6003e666e058
Pedro Henrique Goncalves Pires <pires.phgp@gmail.com>
2023-09-19 15:24:58 -0300

    Coloca metodo de debug no celery.py

--- plataforma_backup/celery.py
+++ plataforma_backup/celery.py
@@ -11,3 +11,8 @@ app = Celery('plataforma_backup')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
