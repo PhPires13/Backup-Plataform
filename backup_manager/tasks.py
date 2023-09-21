6124be48b73f9187b12f494f89d873a9d339a348
Pedro Henrique Gonçalves Pires <pedro.pires@defensoria.mg.def.br>
2023-09-21 11:13:45 -0300

    Modulariza o finish task

--- backup_manager/tasks.py
+++ backup_manager/tasks.py
@@ -8,6 +8,20 @@ from django.utils import timezone
 from backup_manager.models import Database, Backup, Restore, STATUS
 
 
+def start_task(obj):
+    # Set the status and description before running the command
+    obj.set_status(STATUS.STARTED.value)
+    obj.dt_start = timezone.now()
+    obj.save()
+
+
+def finish_task(obj, status: STATUS, description: str):
+    # Set the status and description after a success
+    obj.set_status(status.value)
+    obj.dt_end = timezone.now()
+    obj.save()
+
+
 def database_connect(database: Database, user: str, password: str) -> (psycopg2.extensions.connection, psycopg2.extensions.cursor):
     connection = psycopg2.connect(
         host=database.host.ip,
@@ -22,35 +36,32 @@ def database_connect(database: Database, user: str, password: str) -> (psycopg2.
 
 
 def run_command(obj, command: list, password: str):
-    # Set the status and description before running the command
-    obj.set_status(STATUS.STARTED.value)
-    obj.dt_start = timezone.now()
-    obj.save()
-
+    # Set the postgres password as an environment variable
     os.environ['PGPASSWORD'] = password
 
     # Run the command
     try:
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, input=password, check=True, encoding='utf-8')
         # Set the status and description after a success
-        obj.set_status(STATUS.SUCCESS.value)
-        obj.description = result.stdout
+        status= STATUS.SUCCESS
+        description = result.stdout
     except subprocess.CalledProcessError as e:
         # Set the status and description after a fail
-        obj.set_status(STATUS.FAILED.value)
-        obj.description = e.stderr
+        status = STATUS.FAILED
+        description = e.stderr
     except Exception as e:
         # Set the status and description after a fail
-        obj.set_status(STATUS.FAILED.value)
-        obj.description = str(e)
+        status = STATUS.FAILED
+        description = str(e)
 
-    obj.dt_end = timezone.now()  # Set the end date
-    obj.save()
+    return status, description
 
 
 @shared_task
 def perform_backup(backup_id: int, user: str, password: str):
     backup = Backup.objects.get(id=backup_id)  # Get the backup object
+
+    start_task(backup)
 
     # Verify if the backup is not already done
     if backup.status != STATUS.PENDING.value:
@@ -72,12 +83,16 @@ def perform_backup(backup_id: int, user: str, password: str):
         '--file', backup.complete_path(),
     ]
 
-    run_command(backup, command, password)
+    status, description = run_command(backup, command, password)
+
+    finish_task(backup, status, description)
 
 
 @shared_task
 def perform_restore(restore_id: int, user: str, password: str, to_keep_old_data: bool, to_ignore_public_schema: bool):
     restore = Restore.objects.get(id=restore_id)  # Get the restore object
+
+    start_task(restore)
 
     origin_backup = restore.origin_backup
     destination_database = restore.destination_database
@@ -88,10 +103,7 @@ def perform_restore(restore_id: int, user: str, password: str, to_keep_old_data:
         connection, cursor = database_connect(destination_database, user, password)
     except Exception as e:
         # Set the status and description after a fail
-        restore.set_status(STATUS.FAILED.value)
-        restore.description = str(e)
-        restore.dt_end = timezone.now()
-        restore.save()
+        finish_task(restore, STATUS.FAILED, str(e))
         return
 
     if to_keep_old_data:
@@ -112,10 +124,7 @@ def perform_restore(restore_id: int, user: str, password: str, to_keep_old_data:
                 cursor.execute(f"ALTER SCHEMA {schema_name} RENAME TO {schema_name}_old_{restore.dt_create.strftime('%d_%m_%Y_%H_%M')} ;")
         except Exception as e:
             # Set the status and description after a fail
-            restore.set_status(STATUS.FAILED.value)
-            restore.description = str(e)
-            restore.dt_end = timezone.now()
-            restore.save()
+            finish_task(restore, STATUS.FAILED, str(e))
             return
     else:
         try:
@@ -130,10 +139,7 @@ def perform_restore(restore_id: int, user: str, password: str, to_keep_old_data:
             cursor.execute(f'CREATE DATABASE {destination_database.name} ;')
         except Exception as e:
             # Set the status and description after a fail
-            restore.set_status(STATUS.FAILED.value)
-            restore.description = str(e)
-            restore.dt_end = timezone.now()
-            restore.save()
+            finish_task(restore, STATUS.FAILED, str(e))
             return
 
     try:
@@ -157,4 +163,6 @@ def perform_restore(restore_id: int, user: str, password: str, to_keep_old_data:
         '--file', origin_backup.complete_path(),
     ]
 
-    run_command(restore, command, password)
+    status, description = run_command(restore, command, password)
+
+    finish_task(restore, status, description)
