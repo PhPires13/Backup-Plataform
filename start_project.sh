396dde318e5335b5c60324b065c2ae2e118f329e
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2023-09-29 15:23:17 -0300

    Fix start_project

--- start_project.sh
+++ start_project.sh
@@ -3,10 +3,10 @@ sudo service redis-server start
 
 # Start the Celery worker
 # Might need sudo to manipulate files
-sudo python3.11 manage.py celery worker --loglevel=info &
+sudo celery -A plataforma_backup worker --loglevel=info
 
 # Start the Celery beat
-python3.11 manage.py celery beat --loglevel=info &
+celery -A plataforma_backup beat --loglevel=info
 
 # Start the Django development server
 python3.11 manage.py runserver 0.0.0.0:8000
