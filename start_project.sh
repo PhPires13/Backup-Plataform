9254e5d7568d0fb5ff4c06585f5f2ebfa7ee6273
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2023-09-29 15:30:05 -0300

    Add nohup to comands

--- start_project.sh
+++ start_project.sh
@@ -3,10 +3,10 @@ sudo service redis-server start
 
 # Start the Celery worker
 # Might need sudo to manipulate files
-sudo celery -A plataforma_backup worker --loglevel=info &
+nohup sudo celery -A plataforma_backup worker --loglevel=info &
 
 # Start the Celery beat
-celery -A plataforma_backup beat --loglevel=info &
+nohup celery -A plataforma_backup beat --loglevel=info &
 
 # Start the Django development server
-python3.11 manage.py runserver 0.0.0.0:8000
+nohup python3.11 manage.py runserver 0.0.0.0:8000 &
