2afb62c7f478a1af6a42b9e1da3c7c4857f510bf
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2023-10-05 11:53:47 -0300

    Reorder stop

--- stop_project.sh
+++ stop_project.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+# Start the Django development server
+sudo pkill -f 'python3.11 manage.py runserver 0.0.0.0:8000'
+
 # Stop the Celery worker
 sudo pkill -f 'sudo celery -A plataforma_backup worker --loglevel=info'
 
 # Stop the Celery beat
 sudo pkill -f 'celery -A plataforma_backup beat --loglevel=info'
-
-# Start the Django development server
-sudo pkill -f 'python3.11 manage.py runserver 0.0.0.0:8000'
 
 # Stop the Redis server
 sudo service redis-server stop
