a73af151dc10d8dcfe88f914153e0e9033350980
Pedro Henrique Gonçalves Pires <pires.phgp@gmail.com>
2023-10-17 11:18:18 -0300

    Add tempalates folder to templates disrs

--- plataforma_backup/settings.py
+++ plataforma_backup/settings.py
@@ -68,7 +68,7 @@ ROOT_URLCONF = 'plataforma_backup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
